abd2abb847ee36e70d17cb38d72b818377c9ffe1
David Brown <david@davidmarcbrown.com>
2016-03-29 16:10:31 -0400

    set basic initialization of popup

--- popup.js
+++ popup.js
@@ -18,43 +18,37 @@ function start_button_click() {
 
 }
 
-function pause_button_click() {
-
-}
-
 function reset_button_click() {
 
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var statusdiv = document.getElementById("status");
-
+    // set click handlers
     var startbutton = document.getElementById('startbutton');
     startbutton.addEventListener('click', start_button_click);
-
-    var pausebutton = document.getElementById('pausebutton');
-    pausebutton.addEventListener('click', pause_button_click);
 
     var resetbutton = document.getElementById('resetbutton');
     resetbutton.addEventListener('click', reset_button_click);
 
+    // vars
+    var statusdiv = document.getElementById("status");
+
+    var timeleft;
+
+    // check if alarm has been set.    
     chrome.alarms.get('20MinuteAlarm', function(alarm) {
         if (alarm) {
+            timeleft = new Date(alarm.scheduledTime - Date.now());
             // set the text to the current time remaining
             // and set the timer in motion
-            // take into account paused alarms    
+            // take into account paused alarms
+            if (timeleft.getTime() > 0) {
+                timerStatus(timeleft.getMinutes + ":" + timeleft.getSecond());
+            } else {
+                timerStatus("00:00");
+            }
         } else {
-            // set timer to its last position
-            // or initial position
-            chrome.storage.local.get("timeleft", function(timeleft) {
-                if (!chrome.runtime.lasterror) {
-                    // no previous time saved.
-                    // set display to 20 minutes
-                } else {
-                    // set display to the value
-
-                }
-            });
+            timerStatus("20:00");
         }
     });
 });
